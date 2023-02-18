d951afaddd80694956237ef800a60a3706017b54
Sushant Walunj <88195944+sushantwalunj12@users.noreply.github.com>
2023-02-18 11:26:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,10 @@
 
 function palindrome(str){
 
+	// with reguler expr
+	 str = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  
+  // compare the string with its reverse
+  return str === str.split('').reverse().join('');
 }
 module.exports = palindrome
